e188270c580f2a4957a1587c3183f5abed371456
Brubis <ifb.bruna.599@gmail.com>
2024-04-22 15:14:08 -0300

    organizando algoritmo para n lascar com o codigo
    comentando cada configuração do CSS para quando colocar o carrossel feito não ter desavença de palavras com as Classes e Id's

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+//Carrossel
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector(".container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+
+    // Manipula o arrasto do polegar na barra de rolagem
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+        /*const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;*/
+
+        //Atualiza a posição do polegar ao mover o mouse
+        const handleMouseMove = (e) => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+
+            //Garante que o polegar da barra de rolagem permaneça dentro dos limites
+            const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+            scrollbarThumb.style.left = '${boundedPosition} px';
+            imageList.scrollLeft = scrollPosition;
+        }
+
+        // Remove ouvintes de eventos ao passar o mouse
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        }
+
+        //Adiciona ouvintes de eventos para interação de arrastar
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    });
+
+    // Desliza as imagens de acordo com os cliques do botão do slide
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+    //Mostra ou oculta botões de slide com base na posição de rolagem
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    }
+
+    //Atualiza a posição do polegar da barra de rolagem com base na rolagem da imagem
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = '${thumbPosition} px';
+    }
+
+    // Chame essas duas funções quando a lista de imagens rolar
+    imageList.addEventListener("scroll", () => {
+        updateScrollThumbPosition();
+        handleSlideButtons();
+    });
+}
+
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
+
+//FIM D CARROSSEL
